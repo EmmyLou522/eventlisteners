8c0f75ce370812c11c86159b9fbc7363c4ea20bb
Emily Allen <emmylou522@gmail.com>
2015-02-25 21:45:40 -0500

    create custom javascript event listener

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function showColor(color) {
+    switch( color ) {
+    case "red":
+            document.getElementById("hexcodes").value="#e50000";
+            break;
+    case "orange":
+            document.getElementById("hexcodes").value="#ffa500";
+            break;
+    case "yellow":
+            document.getElementById("hexcodes").value="#f5f36f";
+            break;
+    case "green":
+            document.getElementById("hexcodes").value="#00b200";
+            break;
+    case "blue":
+            document.getElementById("hexcodes").value="#3232ff";
+            break;
+    case "violet":
+            document.getElementById("hexcodes").value="#551a8b";
+            break;
+    }
+}
