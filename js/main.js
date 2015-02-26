8730afb91c3f42e56f99279ea90b1f35213d8bef
Emily Allen <emmylou522@gmail.com>
2015-02-25 20:12:15 -0500

    submit form event listener

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function logForm() {
+    console.log("Firstname was " + document.contactinfo.firstname.value);
+    console.log("Lastname was " + document.contactinfo.lastname.value);
+    console.log("Email was " + document.contactinfo.email.value);
+    console.log("Message was " + document.contactinfo.message.value);
+}
+
